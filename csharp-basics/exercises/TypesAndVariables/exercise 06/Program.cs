bbedfcaafb4a5de5a8d1246fe09097adcf1529a7
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-11-27 15:21:16 +0200

    exercise 6 completed

---
+++ csharp-basics/exercises/TypesAndVariables/exercise 06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercise_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter single digit numbers(Maximum 10 numbers):  ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (number != 0)
+            {
+                int modulus  = number % 10;
+                number /= 10;
+                sum += modulus;
+            }
+            Console.WriteLine($"Sum of all the numbers: {sum}");
+        }
+    }
+}
