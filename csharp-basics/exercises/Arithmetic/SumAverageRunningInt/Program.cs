ca74da411e821aa39145a9dc309cd2be0494a8ae
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-11-27 12:29:07 +0200

    exercise 3 completed

--- csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
+++ csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
@@ -7,7 +7,6 @@ namespace SumAverageRunningInt
         static void Main(string[] args)
         {
             var sum = 0;
-            int average;
             const int lowerBound = 1;
             const int upperBound = 100;
 
@@ -15,6 +14,11 @@ namespace SumAverageRunningInt
             {
                 sum += number;
             }
+
+            double average = (double)sum / upperBound;
+
+            Console.WriteLine($"The sum of {lowerBound} and {upperBound} is {sum}");
+            Console.WriteLine($"The average is {average}");
         }
     }
 }
