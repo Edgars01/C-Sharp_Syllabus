a3df30d6e3a8f24162196e233470bf1383a1e898
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-11-27 15:32:22 +0200

    exercise 7 completed

---
+++ csharp-basics/exercises/TypesAndVariables/exercise 07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercise_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string using Upper and Lower case letters : ");
+            string input = Console.ReadLine();
+            IsItUpper(input);
+        }
+
+        static string IsItUpper(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i])) count++;
+            }
+            Console.WriteLine($"Count of uppercase letters: {count}");
+            return count.ToString();
+        }
+    }
+}
