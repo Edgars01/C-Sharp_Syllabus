61669942d5ae7c7c5ee2aa1d0586e10babf4b91d
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-11-27 11:58:43 +0200

    exercise 2 completed

---
+++ csharp-basics/exercises/Arithmetic/exercise 01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(17, 2));
+            Console.ReadLine();
+        }
+
+        public static bool test(int a, int b)
+        {
+            return a == 15 || b == 15 || a + b == 15 || Math.Abs(a - b) == 15;
+        }
+    }
+}
