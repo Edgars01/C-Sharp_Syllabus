4827867fd3b3fc8279bfe4f1ac3be219c4ccdb55
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-01 15:40:48 +0200

    list exercise 11 completed

--- csharp-basics/exercises/Collections/ListExercise11/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise11/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ListExercise11
 {
@@ -12,17 +9,38 @@ namespace ListExercise11
         {
             //TODO: Create an List with string elements
 
+            List<string> ParticleList = new List<string>();
+
             //TODO: Add 10 values to list
+            ParticleList.Add("Hydrogen");
+            ParticleList.Add("Helium");
+            ParticleList.Add("Oxygen");
+            ParticleList.Add("Carbon");
+            ParticleList.Add("Neon");
+            ParticleList.Add("Nitrogen");
+            ParticleList.Add("Magnesium");
+            ParticleList.Add("Silicon");
+            ParticleList.Add("Iron");
+            ParticleList.Add("Sulfur");
 
             //TODO: Add new value at 5th position
+            ParticleList.Insert(5, "Barium");
 
             //TODO: Change value at last position (Calculate last position programmatically)
+            ParticleList[ParticleList.Count - 1] = "Astatine";
 
             //TODO: Sort your list in alphabetical order
+            ParticleList.Sort();
 
             //TODO: Check if your list contains "Foobar" element
+            Console.WriteLine("Foobar? " + ParticleList.Contains("Foobar") + "\n");
 
             //TODO: Print each element of list using loop
+            foreach (var value in ParticleList)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
         }
     }
 }
