2a67a3d7b0e466ec7352ed8b1f928eac35db6254
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-01 15:22:51 +0200

    list exercise 10 completed

--- csharp-basics/exercises/Collections/ListExercise10/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise10/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ListExercise10
 {
@@ -24,9 +21,12 @@ namespace ListExercise10
 
             //TODO: Remove all elements from list
             //...
+            colors.Clear();
 
             Console.WriteLine("New array list: ");
             Console.WriteLine(string.Join(",", colors));
+
+            Console.ReadKey();
         }
     }
 }
