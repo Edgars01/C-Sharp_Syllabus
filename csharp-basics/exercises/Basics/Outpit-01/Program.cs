1ba29a9285eb257d55f4b682616c6a64dfadc56f
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-01 13:47:43 +0200

    exercise 9 completed

--- csharp-basics/exercises/Basics/Outpit-01/Program.cs
+++ csharp-basics/exercises/Basics/Outpit-01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Output_01
 {
@@ -12,6 +8,7 @@ namespace Output_01
 
         static void Main(string[] args)
         {
+            Console.WriteLine(GetNumber());
             Console.ReadKey();
         }
 
