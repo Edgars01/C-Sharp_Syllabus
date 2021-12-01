2c81c9798f1ae071a7f1b982861d8b1c5687e4b6
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-01 12:29:52 +0200

    exercise 4 both versions completed

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/exercise 4/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace exercise_4
+{
+    internal class Program
+    {
+        private static int value;
+        static void Main(string[] args)
+        {
+            menu();
+        }
+        private static void exit()
+        {
+            Console.Write("\nDo you want to continue (Y/N)? ");
+            Console.WriteLine(" ");
+            char ch = Console.ReadLine()[0];
+            if (ch == 'n' || ch == 'N')
+            {
+                Console.WriteLine("Bye!");
+                Environment.Exit(0);
+            }
+            Console.Clear();
+            menu();
+        }
+
+        private static void menu()
+        {
+            Console.WriteLine("Hi, please choose: \n");
+            Console.WriteLine("   1) a nested -if statement version or ");
+            Console.WriteLine("   2) a switch-case -default statement.");
+            Console.WriteLine("   3) Exit.\n");
+            Console.Write("Enter 1, 2 or 3: ");
+
+            var userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out value))
+            {
+                if (value > 3 || value < 1)
+                { 
+                    Console.WriteLine("Error! Only numbers 1 to 3");
+                    exit();
+                }
+
+                if (value == 1)
+                {
+                    nestedIfChoice();
+                }
+                else if (value == 2)
+                {
+                    switchChoice();
+                }
+                else
+                {
+                    Console.WriteLine("Bye!");
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error! Only numbers 0 to 6!");
+                Console.ReadLine();
+            }
+        }
+
+        private static void nestedIfChoice()
+        {
+            Console.Clear();
+            Console.WriteLine("Hello! Enter 0, 1, ..., 6 for day of week: ");
+            var userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out value))
+            {
+                if (value > 6 || value < 0)
+                {
+                    Console.WriteLine("Not a valid day");
+                }
+                else if (value == 0)
+                {
+                    Console.WriteLine("Monday");
+                }
+                else if (value == 1)
+                {
+                    Console.WriteLine("Tuesday");
+                }
+                else if (value == 2)
+                {
+                    Console.WriteLine("Wednesday");
+                }
+                else if (value == 3)
+                {
+                    Console.WriteLine("Thursday");
+                }
+                else if (value == 4)
+                {
+                    Console.WriteLine("Friday");
+                }
+                else if (value == 5)
+                {
+                    Console.WriteLine("Saturday");
+                }
+                else
+                {
+                    Console.WriteLine("Sunday");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error! Enter number from 0 to 6: ");
+                Console.ReadLine();
+            }
+            exit();
+        }
+
+        private static void switchChoice()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the number from 0 to 6.");
+            var input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 0:
+                    Console.WriteLine("Monday");
+                    break;
+                case 1:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 2:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 4:
+                    Console.WriteLine("Friday");
+                    break;
+                case 5:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 6:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid day.");
+                    break;
+            }
+            exit();
+        }
+    }
+}
