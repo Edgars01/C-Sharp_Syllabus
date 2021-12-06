65b5f746fe6024ea4cd0cecdc02da2c03370e86d
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-06 15:48:42 +0200

    exercise 5 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first word: ");
+            string firstWord = Console.ReadLine();
+            Console.WriteLine("Enter first word: ");
+            string secondWord = Console.ReadLine();
+            Console.Write(firstWord);
+            for (int i = 1; i < 31 - firstWord.Length - secondWord.Length; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write(secondWord);
+            Console.ReadKey();
+        }
+    }
+}
