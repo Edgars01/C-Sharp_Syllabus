b1eed4dd5ff8d9eb85300161ce84e09954580828
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-06 15:09:21 +0200

    exercise 3 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomArr = new int[20];
+            Random rnd = new Random();
+            for (int i = 0; i < randomArr.Length; i++)
+            {
+                randomArr[i] = rnd.Next(1, 21);
+            }
+            Console.WriteLine("Enter position out of 20 random numbers you want to know");
+            int userPos = Convert.ToInt32(Console.ReadLine());
+            if (userPos < 0 || userPos > 21)
+            {
+                 Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine($"{randomArr[userPos]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
