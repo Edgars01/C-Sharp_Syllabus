142d797352e7e2cf90905fc97df1b61d927147f7
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-07 12:38:24 +0200

    exercise 8 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 8 FIGURE/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_8_FIGURE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of figure: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int side = 0;
+            int mid = 0;
+              
+            for (int i = 2; i <= size; i++)
+            {
+                side += 4;              
+            }
+            for (int l = 0; l <= size; l++)
+            {
+                Console.Write(new String('/', side));
+                Console.Write(new String('*', mid));
+                Console.Write(new String('\\', side));
+                Console.WriteLine();
+                side -= 4;
+                mid += 8;
+            }
+            Console.ReadKey();
+        }
+    }
+}
