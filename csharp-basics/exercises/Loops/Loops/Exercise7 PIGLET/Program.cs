d689efc7de09991e62204a567036702ddea4f5d1
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-07 11:20:22 +0200

    exercise 7 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise7 PIGLET/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Exercise7_PIGLET
+{
+    internal class Program
+    {
+        private static Random rnd = new Random();
+        private static int score = 0;
+
+        static void Main(string[] args)
+        {
+
+            play();
+        }
+
+        private static void play()
+        {
+            do
+            {
+                Console.WriteLine("    Roll? Y/N");
+                char ch = Console.ReadLine()[0];
+                if (ch == 'y' || ch == 'Y')
+                {
+                    int roll = rnd.Next(1, 5);
+                    score += roll;
+                    Console.Clear();
+                    Console.WriteLine($"    You rolled : {roll}.");
+                    Console.WriteLine($"    Current score: {score}\n");
+
+                    if (roll == 1)
+                    {
+                        score = 0;
+                        Console.WriteLine($"    GameOver {score}");
+                        exit();
+                    }
+                }
+                else 
+                {
+                    Environment.Exit(0);
+                }
+            } while (score != 0);
+            exit();
+        }
+
+        private static void exit()
+        {
+            Console.Write("    Do you want to continue (Y/N)? ");
+            Console.WriteLine(" ");
+            char ch = Console.ReadLine()[0];
+            if (ch == 'n' || ch == 'N')
+            {
+                Environment.Exit(0);
+            }
+            Console.Clear();
+            play();
+        }
+    }
+}
