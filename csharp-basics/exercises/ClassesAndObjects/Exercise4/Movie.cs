b2dfe741295d22f441fcbefc7e20f5d070f8c5eb
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-08 22:28:26 +0200

    exercise 4 not completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    public class Movie
+    {
+        private string _title;
+        private string _studio;
+        private string _rating;
+
+        public Movie(string title, string studio, int rating)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = "PG";
+        }
+    }
+}
+
+   
