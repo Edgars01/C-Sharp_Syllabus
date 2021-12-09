19c0eebd26776325305791e47301547b7e1d59ae
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-09 15:39:32 +0200

    exercise 5 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 5/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    public class Date
+    {
+        public int _day;
+        public int _month;
+        public int _year;
+
+        public Date(int month, int day, int year)
+        {
+            _day = day;
+            _month = month;
+            _year = year;
+        }
+
+        public int Day
+        { 
+            get
+            {
+                return _day;
+            }
+            set
+            {
+                _day = value;
+            }
+        }
+
+        public int Month
+        {
+            get
+            {
+                return _month;
+            }
+            set
+            {
+                _month = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                _year = value;
+            }
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine($"{_day} / {_month} / {_year}");
+        }
+    }
+}
