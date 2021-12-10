ca6f06a1d885e862f8c6fa2f0e7261bea615168e
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-11 01:59:28 +0200

    TicTacToe better fix

--- csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using static System.Console;
 
 namespace TicTacToe
 {
-    class Program
+    internal class Program
     {
-        public static char[,] board = new char[3, 3];
+        public static char[,] Board = new char[3, 3];
         public static Random Random = new Random();
-        public static bool gameWon = false;
-        public static bool tie = false;
-        public static int countMoves = 0;
-        public static char playerTurn = Random.Next(0, 2) == 0 ? 'X' : 'O';
+        public static bool GameWon = false;
+        public static bool Game_Tie = false;
+        public static int CountMoves = 0;
+        public static char ThisPlayerTurn = Random.Next(0, 2) == 0 ? 'X' : 'O';
+        public static int InputX;
+        public static int InputY;
 
         private static void Main(string[] args)
         {
@@ -18,117 +21,106 @@ namespace TicTacToe
 
         private static void Play()
         {
-            Console.WriteLine($"{playerTurn} starts!\n");
-            InitBoard();
-
+            GameWon = false;
+            Game_Tie = false;
+            CleanUpGameBoard();
+            WriteLine($"{ThisPlayerTurn} starts!\n");
             do
             {
                 DisplayBoard();
-                firstCheckPoint:
-                
-                if (WinnerXO() == 'X' || WinnerXO() == 'O')
-                {
-                    gameWon = true;
-                    Console.WriteLine($"{WinnerXO()} is winner!");
-                    Exit();
-                }
-                else
-                {
-                    Console.Write($"{playerTurn}, choose your location X axis: ");
-                    var inputX = Convert.ToInt32(Console.ReadLine());
-
-                    Console.Write($"{playerTurn}, choose your location Y axis: ");
-                    var inputY = Convert.ToInt32(Console.ReadLine());
-                   
-                    Console.WriteLine();
-
-                    if (0 <= inputY && inputY < 3 && 0 <= inputX && inputX < 3 && board[inputX, inputY] == ' ')
-                    {
-                        board[inputX, inputY] = playerTurn;
-                        playerTurn = playerTurn == 'X' ? 'O' : 'X';
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error try again!\n");
-                        goto firstCheckPoint;
-                    }
-
-                    if (countMoves == 8 && WinnerXO() == '-')
-                    {
-                        tie = true;
-                        Console.WriteLine("Tie!");
-                        Exit();
-                    }
-
-                    countMoves++;
-                    Console.Clear();
-                }
-                WinnerXO();
-            } while (gameWon == false && tie == false);
+                GettingUserInput();
+                ConfirmingUserInput();
+                CountMoves++;
+            } while (!GameWon && !Game_Tie);
         }
 
-        private static void InitBoard()
+        private static void ResetPlayingBoard()
         {
             for (var r = 0; r < 3; r++)           
                 for (var c = 0; c < 3; c++)
-                    board[r, c] = ' ';
+                    Board[r, c] = ' ';
         }
 
-        private static char WinnerXO()
-        {           
-            if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2]) { 
-                return board[0, 0];
+        public static void GettingUserInput()
+        {
+            WriteLine($"{ThisPlayerTurn}, choose your location X axis: ");
+            InputX = Convert.ToInt32(ReadLine());
+            WriteLine($"{ThisPlayerTurn}, choose your location Y axis: ");
+            InputY = Convert.ToInt32(ReadLine());
+            Clear();
+        }
+
+        private static void ConfirmingUserInput()
+        {
+            if (ConditionToConfirmInput())
+            {
+                Board[InputX, InputY] = ThisPlayerTurn;
+                GettingTheWinner(ThisPlayerTurn);
+                ThisPlayerTurn = ThisPlayerTurn == 'X' ? 'O' : 'X';
             }
-            else if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2]) {
-                return board[1, 0];
+            else
+            {
+                WriteLine("Error try again!\n");
+                GettingUserInput();
             }
-            else if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2]) {
-                return board[2, 0];
+        }
+
+        private static void GettingTheWinner(char winnerIsXorO)
+        {
+            if (Board[0, 0] == winnerIsXorO && Board[0, 1] == winnerIsXorO && Board[0, 1] == winnerIsXorO && Board[0, 2] == winnerIsXorO
+                || Board[1, 0] == winnerIsXorO && Board[1, 1] == winnerIsXorO && Board[1, 1] == winnerIsXorO && Board[1, 2] == winnerIsXorO
+                || Board[2, 0] == winnerIsXorO && Board[2, 1] == winnerIsXorO && Board[2, 1] == winnerIsXorO && Board[2, 2] == winnerIsXorO
+                || Board[0, 0] == winnerIsXorO && Board[1, 0] == winnerIsXorO && Board[1, 0] == winnerIsXorO && Board[2, 0] == winnerIsXorO
+                || Board[0, 1] == winnerIsXorO && Board[1, 1] == winnerIsXorO && Board[1, 1] == winnerIsXorO && Board[2, 1] == winnerIsXorO
+                || Board[0, 2] == winnerIsXorO && Board[1, 2] == winnerIsXorO && Board[1, 2] == winnerIsXorO && Board[2, 2] == winnerIsXorO
+                || Board[0, 0] == winnerIsXorO && Board[1, 1] == winnerIsXorO && Board[1, 1] == winnerIsXorO && Board[2, 2] == winnerIsXorO
+                || Board[0, 2] == winnerIsXorO && Board[1, 1] == winnerIsXorO && Board[1, 1] == winnerIsXorO && Board[2, 0] == winnerIsXorO
+               )
+            {
+                GameWon = true;
+                WriteLine($"{ThisPlayerTurn} won!");
+                Exit();
             }
-           
-            else if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0]) { 
-                return board[0, 0];
-            }
-            else if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1]) {
-                return board[0, 1];
-            }
-            else if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2]) {
-                return board[0, 2];
-            }
-            
-            else if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) {
-                return board[0, 0];
-            }
-            else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]) {
-                return board[0, 2];
-            }
-            else {
-                return '-';
+            else if (CountMoves == 9)
+            {
+                Game_Tie = true;
+                WriteLine("Tie");
+                Exit();
             }
         }
 
         private static void Exit()
         {
-            Console.Write("Do you want to continue (Y/N)? ");
-            Console.WriteLine(" ");
-            char ch = Console.ReadLine().ToUpper()[0];
-            if (ch == 'N') {
+            WriteLine("Do you want to continue (Y/N)? ");
+            WriteLine(" ");
+            char ch = ReadLine().ToUpper()[0];
+            if (ch == 'N')
+            {
                 Environment.Exit(0);
             }
-            Console.Clear();
-            InitBoard();
+            ResetPlayingBoard();
             Play();
         }
 
         private static void DisplayBoard()
         {
-            Console.WriteLine("\n\n  0  " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
-            Console.WriteLine("    --+-+--");
-            Console.WriteLine("  1  " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2]);
-            Console.WriteLine("    --+-+--");
-            Console.WriteLine("  2  " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2]);
-            Console.WriteLine("    --+-+--");
-            Console.WriteLine();
-        }   
+            WriteLine("\n\n  0  " + Board[0, 0] + "|" + Board[0, 1] + "|" + Board[0, 2]);
+            WriteLine("    --+-+--");
+            WriteLine("  1  " + Board[1, 0] + "|" + Board[1, 1] + "|" + Board[1, 2]);
+            WriteLine("    --+-+--");
+            WriteLine("  2  " + Board[2, 0] + "|" + Board[2, 1] + "|" + Board[2, 2]);
+            WriteLine("    --+-+--\n");
+        }
+
+        private static void CleanUpGameBoard()
+        {
+            ResetPlayingBoard();
+            Clear();
+        }
+
+        private static bool ConditionToConfirmInput()
+        {
+           return 0 <= InputY && InputY < 3 && 0 <= InputX && InputX < 3 && Board[InputX, InputY] == ' ';
+        }
     }
 }
