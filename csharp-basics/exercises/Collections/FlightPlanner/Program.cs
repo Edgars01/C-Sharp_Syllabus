20adbf1fbcc985957d2a49c27fdde72ac6abeff5
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-12 01:02:53 +0200

    exercise 6 completed

--- csharp-basics/exercises/Collections/FlightPlanner/Program.cs
+++ csharp-basics/exercises/Collections/FlightPlanner/Program.cs
@@ -2,22 +2,130 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace FlightPlanner
 {
-    class Program
+    public class Flights
     {
-        private const string Path = "../FlightPlanner/flights.txt";
+        private const string _path = "../../flights.txt";
+        public static List<string> roundTripFlight = new List<string>();
+        public static string[] readFileFromPath = File.ReadAllLines(_path);
+        public static Dictionary<string, string> possibleRoutes = GetNextFlight(readFileFromPath);
 
-        private static void Main(string[] args)
+        public static string[][] array = File.ReadAllLines(_path)
+            .Select(x => x.Replace(" -> ", "-").Split('-'))
+            .ToArray();
+
+        public static void Main(string[] args)
         {
-            var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+            TravelOrExit();
+            WriteLine("\nWhere you gonna start?");
+            var flyTo = UserChoice();
+            var flyFrom = flyTo;
+
+            do
             {
-                Console.WriteLine(s);
+                roundTripFlight.Add(flyTo);
+                Write("\nSelect the next city : ");
+                WriteLine(possibleRoutes[flyTo]);
+                Write("\nSelect the next city : ");
+                var userChoice = UserChoice();
+                flyTo = userChoice;
+            } while (flyFrom != flyTo);
+
+            WriteLine($"\nroute that was chosen: {string.Join(" then ", roundTripFlight)} to {flyTo}");
+            ReadKey();
+        }
+
+        public static Dictionary<string, string> GetNextFlight(string[] flights)
+        {
+            var routes = new Dictionary<string, string>();
+            foreach (var route in flights)
+            {
+                var citiesFromTxt = route.Split('-');
+                var cityToAdd = citiesFromTxt[0].Trim();
+                var destinationToAdd = citiesFromTxt[1].Trim();
+
+                if (routes.ContainsKey(cityToAdd))
+                    routes[cityToAdd] += destinationToAdd;
+                else
+                    routes.Add(cityToAdd, destinationToAdd);
             }
+
+            return routes;
+        }
+
+        private static void TravelOrExit()
+        {
+            var dictionary = new Dictionary<int, string>();
+            while (true)
+            {
+                WriteLine(" What would you like to do:\n");
+                WriteLine(" To display list of the cities press 1");
+                WriteLine(" To exit program press #");
+                var input = ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        DisplayCities(dictionary, array);
+                        break;
+                    case "#":
+                        WriteLine(" Exiting...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        WriteLine(" Wrong input, try again!");
+                        continue;
+                }
+
+                break;
+            }
+        }
+
+        public static void DisplayCities(Dictionary<int, string> dictionary, string[][] array)
+        {
+            var cities = new HashSet<string>();
+            foreach (var city in array) cities.Add(city[0]);
+            var numbersForCase = cities.Count();
+
+            var citiesWithNoDuplicates = new string[cities.Count];
+            cities.CopyTo(citiesWithNoDuplicates);
+
+            for (var i = 0; i < citiesWithNoDuplicates.Length; i++) dictionary.Add(i, citiesWithNoDuplicates[i]);
+            foreach (var city in dictionary) WriteLine($"{city.Key}:{city.Value}");
+        }
+
+        public static string UserChoice()
+        {
+            var userMadeChoice = "";
+            var ChooseCity = Convert.ToInt32(ReadLine());
+            switch (ChooseCity)
+            {
+                case 0:
+                    userMadeChoice = "San Jose";
+                    break;
+                case 1:
+                    userMadeChoice = "New York";
+                    break;
+                case 2:
+                    userMadeChoice = "Anchorage";
+                    break;
+                case 3:
+                    userMadeChoice = "Honolulu";
+                    break;
+                case 4:
+                    userMadeChoice = "Denver";
+                    break;
+                case 5:
+                    userMadeChoice = "San Francisco";
+                    break;
+                default:
+                    WriteLine(" Wrong input, try again!");
+                    break;
+            }
+
+            return userMadeChoice;
         }
     }
 }
