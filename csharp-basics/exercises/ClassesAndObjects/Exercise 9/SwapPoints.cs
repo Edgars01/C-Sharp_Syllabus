965f86394a0fc50fd915a2d87538ea627523956d
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-11 10:11:07 +0200

    Exercise 9 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 9/SwapPoints.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise_9
+{
+    public class PointsToSwap
+    {
+        public int _x;
+        public int _y;
+
+        public PointsToSwap(int x, int y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        public static void Swap(ref PointsToSwap x, ref PointsToSwap y)
+        {
+            (x, y) = (y, x);
+        }
+    }
+}
