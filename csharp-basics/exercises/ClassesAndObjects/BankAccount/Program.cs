a35693f3c331d5bb7df8320fbde36f239cd76379
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-11 12:53:40 +0200

    exercise 10 completed

--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -8,8 +8,17 @@ namespace BankAccount
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var user19338281 = new BankAccount("Edgars", (decimal)50000);
+            var user19338282 = new BankAccount("Sragde", (decimal)-2);
+            var user19338283 = new BankAccount("Degsra", (decimal)500000.50);
+            var user19338284 = new BankAccount("Gderas", (decimal)0.00);
+            Console.WriteLine(user19338281.ShowUserNameAndBalance());
+            Console.WriteLine(user19338282.ShowUserNameAndBalance());
+            Console.WriteLine(user19338283.ShowUserNameAndBalance());
+            Console.WriteLine(user19338284.ShowUserNameAndBalance());
+            Console.ReadKey();
         }
     }
 }
