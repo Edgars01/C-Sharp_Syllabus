1eb77dc78034b32c174b7c51c5300eb6bb3eebf7
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-12 16:30:57 +0200

    exercise 9 fix

--- csharp-basics/exercises/Loops/exercise 9/Program.cs
+++ csharp-basics/exercises/Loops/exercise 9/Program.cs
@@ -11,20 +11,18 @@ namespace RollTwoDice
             {
                 Console.WriteLine("LETS ROLL DICE!");
                 Console.WriteLine("Desired Sum : ");
-                int desiredSum = Convert.ToInt32(Console.ReadLine());
+                var desiredSum = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"Desired Sum ({desiredSum}) ");
-                Random rnd = new Random();
-                int dice = rnd.Next(1, 7);
-                int diceTwo = rnd.Next(1, 7);
+                var rnd = new Random();
+                var dice = rnd.Next(1, 7);
+                var diceTwo = rnd.Next(1, 7);
                 Console.WriteLine($"{dice} and {diceTwo}");
-                if (dice + diceTwo == desiredSum)
-                {
-                    Console.WriteLine("You Won!");
-                }
+                if (dice + diceTwo == desiredSum) Console.WriteLine("You Won!");
                 Console.WriteLine("Again? y/n");
-                string answer = Console.ReadLine();
+                var answer = Console.ReadLine();
                 Console.Clear();
             } while (answer != "N" || answer != "n");
+
             Environment.Exit(0);
         }
     }
