510bacbe3e187feb9cf05b7687df610fb7cd452c
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2021-12-13 14:27:22 +0200

    exercise 3 completed

---
+++ csharp-basics/exercises/Polymorphism/Persons/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Persons
+{
+    public class Person
+    {
+        public Person(string name, string lastName, string address, int id)
+        {
+            FirstName = name;
+            LastName = lastName;
+            Address = address;
+            Id = id;
+        }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public string Address { get; }
+
+        public int Id { get; }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, lives at {Address}. ID: {Id}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Persons/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Persons
+{
+    public class Student : Person
+    {
+        public Student(string name, string lastName, string address, int id, double gpa) : base(name, lastName, address,
+            id)
+
+        {
+            Gpa = gpa;
+        }
+
+        public double Gpa { get; }
+
+        public override void Display()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, lives at {Address}, have gpa of {Gpa} points. ID: {Id}");
+        }
+    }
+}
