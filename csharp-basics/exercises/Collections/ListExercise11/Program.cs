89833356385bf02e6232f83df0c9017633a3421e
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-01-03 21:43:57 +0200

    ListExercise11 fix

--- csharp-basics/exercises/Collections/ListExercise11/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise11/Program.cs
@@ -7,11 +7,8 @@ namespace ListExercise11
     {
         static void Main(string[] args)
         {
-            //TODO: Create an List with string elements
-
             List<string> ParticleList = new List<string>();
 
-            //TODO: Add 10 values to list
             ParticleList.Add("Hydrogen");
             ParticleList.Add("Helium");
             ParticleList.Add("Oxygen");
@@ -23,19 +20,11 @@ namespace ListExercise11
             ParticleList.Add("Iron");
             ParticleList.Add("Sulfur");
 
-            //TODO: Add new value at 5th position
             ParticleList.Insert(5, "Barium");
-
-            //TODO: Change value at last position (Calculate last position programmatically)
             ParticleList[ParticleList.Count - 1] = "Astatine";
-
-            //TODO: Sort your list in alphabetical order
             ParticleList.Sort();
-
-            //TODO: Check if your list contains "Foobar" element
             Console.WriteLine("Foobar? " + ParticleList.Contains("Foobar") + "\n");
 
-            //TODO: Print each element of list using loop
             foreach (var value in ParticleList)
             {
                 Console.WriteLine(value);
