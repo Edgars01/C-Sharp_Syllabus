ca8f0a4eb46ed3ce906827564ca5428553e0a1f1
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-01-03 21:42:21 +0200

    ListExercise 1 fix

--- csharp-basics/exercises/Collections/ListExercise1/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise1/Program.cs
@@ -8,10 +8,8 @@ namespace ListExercise1
     {
         private static void Main(string[] args)
         {
-            //TODO: Write a C# program to create a new list, add some elements (string) and print out the collection.
             var colors = new List<string>();
 
-            //TODO: Add 5 colors to list
             colors.Add("Red");
             colors.Add("Dark Red");
             colors.Add("Scarlet Red");
