952d97fd393a766b008fcc42a460120b0eedaf52
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-01-07 15:28:23 +0200

    Exercise 5 Test complete

---
+++ csharp-basics/exercises/Tests/Collections/Exercise5/Exercise5/Exercise5/Exercise.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Exercise5
+{
+    public class Exercise : IExercise
+    {
+        public int CharCountInFile(string text)
+        {
+            if (text == null) throw new FileNotFoundException();
+
+            return text.Length;
+        }
+
+        public int LineCount(string text)
+        {
+            return text.Split('\n').Length;
+        }
+
+        public int WordCount(string text)
+        {
+            var word = 0;
+
+            for (var i = 0; i <= text.Length - 1; i++)
+                if (text[i] == ' ' || text[i] == '\n' || text[i] == '\t')
+                    word++;
+
+            return word;
+        }
+    }
+}
