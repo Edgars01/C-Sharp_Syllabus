4c0c9f4ddc7e7cd933c67f2ca3ae3ba8f1d95013
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-01-09 18:18:09 +0200

    Exercise5 Tests Completed

---
+++ csharp-basics/exercises/Tests/ClassesAndObjects/Exercise 5/Exercise5/Date.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Date.Tests.Exception;
+using NUnit.Framework;
+
+namespace Date.Tests
+{
+    public class Tests
+    {
+        private Exercise5.Date _target;
+
+        [SetUp]
+        public void Setup()
+        {
+            _target = new Exercise5.Date(1, 1, 1);
+        }
+
+        [Test]
+        public void ShouldBeAbleToShowDate()
+        {
+            const string expected = "1 / 1 / 1";
+
+            var actual = _target.DisplayDate();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase(1212, 12, 0)]
+        [TestCase(13331, -111, 11111)]
+        [TestCase(333, 1, 9090)]
+        public void ShouldBeAbleToShowExceptionOnWrongDateInput(int month, int day, int year)
+        {
+            Assert.Throws<InvalidDateException>(() => new Exercise5.Date(-1, -1, -1));
+        }
+
+
+
+
+
+    }
+}
---
+++ csharp-basics/exercises/Tests/ClassesAndObjects/Exercise 5/Exercise5/Exercise5/Date.cs
@@ -0,0 +1,25 @@
+﻿using Date.Tests.Exception;
+
+namespace Exercise5
+{
+    public class Date
+    {
+        public int _day, _month, _year;
+
+        public Date(int month, int day, int year)
+        {
+            if (day is < 1 or > 31 || month is < 1 or > 12 || year is < 1 or > 2022)
+                throw new InvalidDateException();
+
+            _day = day;
+            _month = month;
+            _year = year;
+        }
+
+        public string DisplayDate()
+        {
+            return $"{_day} / {_month} / {_year}";
+        }
+    }
+}
+
