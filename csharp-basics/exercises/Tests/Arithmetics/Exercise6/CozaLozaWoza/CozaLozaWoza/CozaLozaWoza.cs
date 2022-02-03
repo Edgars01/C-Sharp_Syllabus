fd9c6cd119be3a09d66b80b246ccd62420edaa48
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-02-03 10:22:56 +0200

    Exercise 6 Test Fix

--- csharp-basics/exercises/Tests/Arithmetics/Exercise6/CozaLozaWoza/CozaLozaWoza/CozaLozaWoza.cs
+++ csharp-basics/exercises/Tests/Arithmetics/Exercise6/CozaLozaWoza/CozaLozaWoza/CozaLozaWoza.cs
@@ -16,16 +16,11 @@ namespace CozaLozaWoza
         {
             if (number % 3 == 0 && number % 5 == 0) return "CozaLoza ";
             if (number % 5 == 0 && number % 7 == 0) return "LozaCoza ";
-            if (number % 3 == 0)
-                return "Coza ";
-            if (number % 5 == 0)
-                return "Loza ";
-            if (number % 7 == 0)
-                return "Woza ";
-            if (number % 11 == 0)
-            {
-                return Environment.NewLine + number;
-            }
+            if (number % 3 == 0) return "Coza ";
+            if (number % 5 == 0) return "Loza ";
+            if (number % 7 == 0) return "Woza ";
+            if (number % 11 == 0) return Environment.NewLine + number;
+        
             return number + " ";
         }
     }
