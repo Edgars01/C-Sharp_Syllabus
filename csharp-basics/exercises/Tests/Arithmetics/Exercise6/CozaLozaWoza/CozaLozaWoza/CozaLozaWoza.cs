d26a9f6b5c8159a21eb53e7d3031c85002dd3e78
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-02-03 10:26:07 +0200

    Exercise 6 Test Fix making if statements into else if

--- csharp-basics/exercises/Tests/Arithmetics/Exercise6/CozaLozaWoza/CozaLozaWoza/CozaLozaWoza.cs
+++ csharp-basics/exercises/Tests/Arithmetics/Exercise6/CozaLozaWoza/CozaLozaWoza/CozaLozaWoza.cs
@@ -15,11 +15,11 @@ namespace CozaLozaWoza
         public string Conditions(int number)
         {
             if (number % 3 == 0 && number % 5 == 0) return "CozaLoza ";
-            if (number % 5 == 0 && number % 7 == 0) return "LozaCoza ";
-            if (number % 3 == 0) return "Coza ";
-            if (number % 5 == 0) return "Loza ";
-            if (number % 7 == 0) return "Woza ";
-            if (number % 11 == 0) return Environment.NewLine + number;
+            else if (number % 5 == 0 && number % 7 == 0) return "LozaCoza ";
+            else if(number % 3 == 0) return "Coza ";
+            else if(number % 5 == 0) return "Loza ";
+            else if(number % 7 == 0) return "Woza ";
+            else if(number % 11 == 0) return Environment.NewLine + number;
         
             return number + " ";
         }
