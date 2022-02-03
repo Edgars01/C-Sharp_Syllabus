00ea4f2372bb5d61ad21e0387463f11bb5b6facf
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-02-03 10:51:36 +0200

    Exercise 7 Test Fix

--- csharp-basics/exercises/Tests/Arithmetics/Exercise7/Exercise7/Exercise7.Tests/Tests.cs
+++ csharp-basics/exercises/Tests/Arithmetics/Exercise7/Exercise7/Exercise7.Tests/Tests.cs
@@ -13,44 +13,56 @@ namespace Exercise7.Tests
         }
 
         [Test]
-        public void GravityValueShouldBe_SameAsExpected()
+        public void GravityValue_ShouldBeSameAsExpectedValue()
         {
+            //Arrange
             var expected = -9.81;
+            //Assert
             Assert.AreEqual(expected, _target.Gravity());
         }
 
         [Test]
-        public void InitialVelocityValueShouldBe_SameAsExpected()
+        public void InitialVelocity_ShouldBeSameAsExpected()
         {
+            //Arrange
             var expected = 0.0;
+            //Assert
             Assert.AreEqual(expected, _target.InitialVelocity());
         }
 
         [Test]
-        public void FallingTimeValueShouldBe_SameAsExpected()
+        public void FallingTimeValue_ShouldBeSameAsExpected()
         {
+            //Arrange
             var expected = 10.0;
+            //Assert
             Assert.AreEqual(expected, _target.FallingTime());
         }
 
         [Test]
-        public void InitialPositionShouldBe_SameAsExpected()
+        public void InitialPosition_ShouldBeSameAsExpected()
         {
+            //Arrange
             var expected = 0.0;
+            //Assert
             Assert.AreEqual(expected, _target.InitialPosition());
         }
 
         [Test]
-        public void PositionFallingForTenSecondsShouldBe_SameAsExpected()
+        public void PositionFallingForTenSeconds_ShouldBeSameAsExpected()
         {
+            //Arrange
             var expected = -490.5;
+            //Assert
             Assert.AreEqual(expected, _target.FinalPositionAfterTenSec());
         }
 
         [Test]
-        public void PositionFallingFor51SecondsShouldBe_SameAsExpected()
+        public void PositionFallingFor51Seconds_ShouldBeSameAsExpected()
         {
+            //Arrange
             var expected = -12757.905;
+            //Assert
             Assert.AreEqual(expected, _target.FinalPositionAfter51Sec());
         }
     }
