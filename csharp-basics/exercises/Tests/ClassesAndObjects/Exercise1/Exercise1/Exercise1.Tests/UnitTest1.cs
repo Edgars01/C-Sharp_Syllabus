bf45d093450ade3ef5caa78ed85f21ea2db47b33
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-02-04 10:16:57 +0200

    Exercise 1 Test method name fix

--- csharp-basics/exercises/Tests/ClassesAndObjects/Exercise1/Exercise1/Exercise1.Tests/UnitTest1.cs
+++ csharp-basics/exercises/Tests/ClassesAndObjects/Exercise1/Exercise1/Exercise1.Tests/UnitTest1.cs
@@ -33,7 +33,7 @@ namespace Exercise1.Tests
         }
 
         [Test]
-        public void ShouldThrowErrorIfNameIsNotProvided()
+        public void PrintName_ShouldThrowErrorIfNameIsNotProvided()
         {
             //Arrange 
             var name = new Product(null, 8050, 1);
@@ -43,7 +43,7 @@ namespace Exercise1.Tests
         }
 
         [Test]
-        public void ShouldThrowErrorIfPriceIsNotProvided()
+        public void PrintPrice_ShouldThrowErrorIfPriceIsNotProvided()
         {
             //Arrange 
             var price = new Product("3-hydroxy-5-aminomethyl-1-isoxazole", 0, 1);
@@ -53,7 +53,7 @@ namespace Exercise1.Tests
         }
 
         [Test]
-        public void ShouldThrowErrorIfQuantityIsNotProvided()
+        public void PrintQuantity_ShouldThrowErrorIfQuantityIsNotProvided()
         {
             //Arrange 
             var quantity = new Product("Dinitrogen trioxide", 50, 0);
@@ -63,51 +63,53 @@ namespace Exercise1.Tests
         }
 
         [Test]
-        public void ShouldBeAbleToChangeName()
+        public void PrintName_ShouldBeAbleToChangeName()
         {
+            //Arrange 
             var name = new Product("BLACK MATTER", 1000000000000, 0.0000001);
-
             var expectedName = "BLACK MATTER";
 
+            //Assert
             Assert.AreEqual(expectedName, name.PrintName());
         }
 
         [Test]
-        public void ShouldThrowErrorIfNameIsNotProvidedWhileChangingValue()
+        public void ChangeName_ShouldThrowErrorIfNameIsNotProvidedWhileChangingValue()
         {
             //Assert
             Assert.Throws<OneOfValuesNotProvidedException>(() => _target.ChangeName(null));
         }
 
         [Test]
-        public void ShouldBeAbleToChangePrice()
+        public void PrintPrice_ProvidingCorrectData_ExpectedPriceShouldBe1000000000000()
         {
+            //Arrange
             var name = new Product("BLACK MATTER", 1000000000000, 0.0000001);
-
             var expectedPrice = 1000000000000;
 
+            //Assert
             Assert.AreEqual(expectedPrice, name.PrintPrice());
         }
 
         [Test]
-        public void ShouldThrowErrorIfPriceIsNotProvidedWhileChangingValue()
+        public void ChangePrice_PriceZero_ShouldFail()
         {
             //Assert
             Assert.Throws<OneOfValuesNotProvidedException>(() => _target.ChangePrice(0));
         }
 
         [Test]
-        public void ShouldBeAbleToChangeQuantity()
+        public void PrintQuantity_ProvidingSuitableData_ExpectedQuantityShouldBe_0Point0000001()
         {
+            //Arrange
             var name = new Product("BLACK MATTER", 1000000000000, 0.0000001);
-
             var expectedQuantity = 0.0000001;
-
+            //Assert
             Assert.AreEqual(expectedQuantity, name.PrintQuantity());
         }
 
         [Test]
-        public void ShouldThrowErrorIfQuantityIsNotProvidedWhileChangingValue()
+        public void ChangeQuantity_AmountZero_ShouldFail()
         {
             //Assert
             Assert.Throws<OneOfValuesNotProvidedException>(() => _target.ChangeQuantity(0));
