8c5928a7ccac9afaa55c9832db21eaaf6e209019
Edgars Svarups <79999452+Edgars01@users.noreply.github.com>
2022-02-05 10:07:15 +0200

    Exercise IFifteen Test new line fix

--- csharp-basics/exercises/Tests/Arithmetics/IsFifteen/Fifteen/Fifteen/IFifteen.cs
+++ csharp-basics/exercises/Tests/Arithmetics/IsFifteen/Fifteen/Fifteen/IFifteen.cs
@@ -3,6 +3,7 @@
     public interface IFifteen
     {
         void CheckingIfNumbersProvidedAreSmallerThanZero_OrTheirSumExceeds_15(int a, int b);
+
         bool CheckingIfProvidedNumberSumIsEqualTo_15(int a, int b);
 
         bool CheckingIfProvidedNumberMinusOtherNumberIsEqualTo_15(int a, int b);
